04e57d65e6eae2326f12c83d82e22c0503aa932a
Vizor777 <pipkamax1993@gmail.com>
2019-03-23 23:20:41 +0400

    23.03.19
    Правки для финала

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var but = document.querySelector(".selection-button");
+
+var popup = document.querySelector(".form-fone");
+
+but.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.toggle("display-show");
+});
+
+
+
+
+
+	
