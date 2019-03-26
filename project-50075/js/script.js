971cb79547cd2b722426b2e1cd788eece454acd4
Vizor777 <pipkamax1993@gmail.com>
2019-03-26 06:41:36 +0400

    Защита-2

--- project-50075/js/script.js
+++ project-50075/js/script.js
@@ -3,31 +3,31 @@ var popup = document.querySelector(".form-fone");
 var date_come = popup.querySelector("[name=coming]");
 var date_dep = popup.querySelector("[name=departure]");
 var gro = popup.querySelector("[name=grown]");
-var babie = popup.querySelector("[name=babies]");
+var	babie = popup.querySelector("[name=babies]");
 
 but.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classList.toggle("display-show");
-    date_come.focus();
+	popup.classList.toggle("display-show");
+	date_come.focus();
 });
 
 popup.addEventListener("submit", function(evt) {
     if (!date_come.value || !date_dep.value || !gro.value || !babie.value) {
-        evt.preventDefault();
-        popup.classList.remove("display-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("display-error");
-    }
+		evt.preventDefault();
+		popup.classList.remove("display-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("display-error");
+}
 });
 
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
-        if (popup.classList.contains("display-show")) {
-            evt.preventDefault();
-            popup.classList.remove("display-show");
-            popup.classList.remove("display-error");
-        }
-    }
+		if (popup.classList.contains("display-show")) {
+			evt.preventDefault();
+			popup.classList.remove("display-show");
+			popup.classList.remove("display-error");
+}
+	}
 });
 
 
